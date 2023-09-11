e263f92b77531c47a090e2a15c61b22a00482241
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2023-09-11 17:13:25 +0200

    create empty java app

---
+++ Java/BabySteps/src/test/java/org/babysteps/MainTest.java
@@ -0,0 +1,13 @@
+package org.babysteps;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void Test_the_truth() {
+        assertEquals(true,true);
+    }
+}
